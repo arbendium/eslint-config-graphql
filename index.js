74d7f0b0deabdad72ade786057dca6c8a44714d4
Keijo Kapp <keijo.kapp@gmail.com>
2024-12-23 23:36:03 +0200

    Reordered rules

--- index.js
+++ index.js
@@ -12,7 +12,16 @@ export default [{
 		graphql: graphqlPlugin
 	},
 	rules: {
+		'graphql/alphabetize': [
+			'error',
+			{
+				fields: ['ObjectTypeDefinition', 'InterfaceTypeDefinition', 'InputObjectTypeDefinition'],
+				values: true,
+				arguments: ['FieldDefinition', 'Field', 'DirectiveDefinition', 'Directive']
+			}
+		],
 		'graphql/description-style': 'error',
+		'graphql/input-name': 'error',
 		'graphql/known-argument-names': 'error',
 		'graphql/known-directives': 'error',
 		'graphql/known-type-names': 'error',
@@ -30,25 +39,16 @@ export default [{
 		],
 		'graphql/no-unreachable-types': 'error',
 		'graphql/provided-required-arguments': 'error',
+		'graphql/require-deprecation-date': 'error',
 		'graphql/require-deprecation-reason': 'error',
-		'graphql/unique-directive-names': 'error',
+		'graphql/require-nullable-fields-with-oneof': 'error',
+		'graphql/require-type-pattern-with-oneof': 'error',
+		'graphql/type-usage-order': 'error',
 		'graphql/unique-directive-names-per-location': 'error',
+		'graphql/unique-directive-names': 'error',
 		'graphql/unique-enum-value-names': 'error',
 		'graphql/unique-field-definition-names': 'error',
 		'graphql/unique-operation-types': 'error',
-		'graphql/unique-type-names': 'error',
-		'graphql/alphabetize': [
-			'error',
-			{
-				fields: ['ObjectTypeDefinition', 'InterfaceTypeDefinition', 'InputObjectTypeDefinition'],
-				values: true,
-				arguments: ['FieldDefinition', 'Field', 'DirectiveDefinition', 'Directive']
-			}
-		],
-		'graphql/input-name': 'error',
-		'graphql/require-deprecation-date': 'error',
-		'graphql/require-nullable-fields-with-oneof': 'error',
-		'graphql/require-type-pattern-with-oneof': 'error',
-		'graphql/type-usage-order': 'error'
+		'graphql/unique-type-names': 'error'
 	}
 }];
